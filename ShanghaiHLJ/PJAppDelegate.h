e4a7e74297f0a65de26391c1a42e0e990ebf141c
13020109262@163.com <13020109262@163.com>
2014-08-01 20:18:09 +0800

    Initial Commit

---
+++ ShanghaiHLJ/PJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PJAppDelegate.h
+//  ShanghaiHLJ
+//
+//  Created by Anne Pan on 14-8-1.
+//  Copyright (c) 2014年 com.pa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShanghaiHLJTests/ShanghaiHLJTests.m
@@ -0,0 +1,34 @@
+//
+//  ShanghaiHLJTests.m
+//  ShanghaiHLJTests
+//
+//  Created by Anne Pan on 14-8-1.
+//  Copyright (c) 2014年 com.pa. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShanghaiHLJTests : XCTestCase
+
+@end
+
+@implementation ShanghaiHLJTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
